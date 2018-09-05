0e284d3b3ede3d22eba3dd114ad0c67bec8440d0
Vassili Bykov <32407917+vassilibykov@users.noreply.github.com>
2018-09-04 23:45:00 -0700

    Messing around

--- src/main/scala/tapl/ch22/IncrementalChecker.scala
+++ src/main/scala/tapl/ch22/IncrementalChecker.scala
@@ -1,16 +1,17 @@
 package tapl.ch22
 
 import tapl.base.ExtendedLambdaCalculus._
-import tapl.base._
+import tapl.base.{TBool, _}
 import tapl.ch10.TypedAbs
 
 import scala.util.{Failure, Success, Try}
 
-/** Type reconstructing checker incrementally unifying the generated constraints
- * (Exercise 22.5.7). The core structure is along the lines of Hindley-Milner
- * algorithm W, so that the type inference function returns the type of the
- * term together with variable substitutions performed to produce it, and which
- * must be applied to all types "upstream" of the invocation.
+/**
+  * Type reconstructing checker incrementally unifying the generated constraints
+  * (Exercise 22.5.7). The core structure is along the lines of Hindley-Milner
+  * algorithm W (I guess), so that the type inference function returns the type
+  * of the term together with variable substitutions performed to produce it,
+  * and which must be applied to all types "upstream" of the invocation.
   */
 object IncrementalChecker {
 
@@ -21,53 +22,48 @@ object IncrementalChecker {
     case Var(name) => (context(name), Substitution.empty)
     case _: BoolLiteral => (TBool, Substitution.empty)
     case _: IntLiteral => (TInt, Substitution.empty)
-    case Succ(expr) =>
-      val (t, sub) = typeOf(expr, context)
-      (TInt, sub.unify(t, TInt))
-    case IsZero(expr) =>
-      val (t, sub) = typeOf(expr, context)
-      (TInt, sub.unify(t, TBool))
-    case TypedAbs(Var(varName), varType, body) =>
+    case Succ(expr) => {
+      val (eType, eSub) = typeOf(expr, context)
+      (TInt, eSub composedWith unify(eType, TInt))
+    }
+    case IsZero(expr) => {
+      val (eType, eSub) = typeOf(expr, context)
+      (TBool, eSub composedWith unify(eType, TInt))
+    }
+    case TypedAbs(Var(varName), varType, body) => {
       val bodyContext = context + (varName -> varType)
-      val (t, sub) = typeOf(body, bodyContext)
-      (TFun(sub(varType), t), sub)
-    // DEFINITELY NOT CORRECT PAST THIS POINT
-    case App(fun, arg) => // bogus, just messing around
-      val (atype, asub) = typeOf(arg, context)
+      val (bType, bSub) = typeOf(body, bodyContext)
+      (TFun(bSub(varType), bType), bSub)
+    }
+    case App(fun, arg) => {
+      val resultType = TVar.generate()
       val (ftype, fsub) = typeOf(fun, context)
-      val tv = TVar.generate()
-      val aug = (asub + fsub).unify(ftype, TFun(atype, tv))
-      (aug(tv), aug)
-    case If(guard, trueBranch, falseBranch) =>
+      val (atype, asub) = typeOf(arg, context)
+      val u = unify(ftype, TFun(atype, resultType))
+      val combined = asub composedWith (fsub composedWith u)
+      (combined(resultType), combined)
+    }
+    case If(guard, trueBranch, falseBranch) => {
       val (gtype, gsub) = typeOf(guard, context)
       val (ttype, tsub) = typeOf(trueBranch, context)
       val (ftype, fsub) = typeOf(falseBranch, context)
-      val gType = TVar.generate()
-      val rType = TVar.generate()
-      val combined = gsub + tsub + fsub + (gType -> TBool) + (rType -> ttype) + (rType -> ftype)
-      (combined(rType), combined)
+      val u1 = unify(ttype, ftype)
+      val u2 = unify(gtype, TBool)
+      val combined = u1 composedWith (u2 composedWith (gsub composedWith (tsub composedWith fsub)))
+      (combined(ttype), combined)
+    }
     case _ => throw new InferenceError(s"Inference not implemented for term: $term")
   }
 
-  private def add(substs: VarSubst, t1: Type, t2: Type): Either[String, VarSubst] = t1 match {
-    case tv: TVar =>
-      if (substs contains tv) {
-        if (substs(tv) == t2) Right(substs)
-        else Left(s"conflicting inferred types for $tv")
-      } else {
-        Right(substs + (tv -> t2))
-      }
-    case _ if t1 == t2 => Right(substs)
-    case _ => Left(s"types $t1 and $t2 are not unifiable")
+  private def unify(t: Type, s: Type): Substitution = (t, s) match {
+    case (TBool, TBool) => Substitution.empty
+    case (TInt, TInt) => Substitution.empty
+    case (x, y) if x == y => Substitution.empty
+    case (v: TVar, t) => Substitution(v, t)
+    case (t, v: TVar) => Substitution(v, t)
+    case (TFun(f1, t1), TFun(f2, t2)) => unify(f1, f2) composedWith unify(t1, t2)
+    case _ => throw new InferenceError(s"can't unify $t and $s")
   }
-
-  private def apply(subst: VarSubst, t: Type): Type = t match {
-    case tv: TVar => subst.getOrElse(tv, t)
-    case TFun(from, to) => TFun(apply(subst, from), apply(subst, to))
-    case _ => t
-  }
-
-  private def union(s1: VarSubst, s2: VarSubst): Either[String, VarSubst] = ???
 
   /**
     * Create a typed abstraction from typeless syntax, with a fresh type variable as the type.
@@ -87,9 +83,16 @@ object IncrementalChecker {
   private def printTypeOf(expr: Term): Unit = {
     println(expr)
     Try { typeOf(expr, Map()) } match {
-      case Success((t, sub)) => println(s"\tprincipal type: $t")
+      case Success((t, sub)) =>
+        println(s"\ttype: $t")
+        println(s"\tsubstitution: $sub")
       case Failure(ex) => println(s"\terror: ${ex.getMessage}")
     }
+  }
+
+  def trace[T](x: T): T = {
+    println("TRACING: " + x)
+    x
   }
 
   class InferenceError(message: String) extends Exception(message)
--- src/main/scala/tapl/ch22/Substitution.scala
+++ src/main/scala/tapl/ch22/Substitution.scala
@@ -3,40 +3,32 @@ package tapl.ch22
 import tapl.base.{TFun, Type}
 
 /**
-  * A set of type variable substitutions.
+  * A mapping from type variables to types.
   */
 class Substitution private (val map: Map[TVar, Type] ) {
-
-  def + (binding: (TVar, Type)): Substitution = {
-    val (tvar, t) = binding
-    map.get(tvar) match {
-      case Some(t2) =>
-        if (t == t2) this
-        else throw new IncrementalChecker.InferenceError(s"$tvar binding conflict: $t2 and $t")
-      case None =>
-        val rewriter = Substitution(tvar, t)
-        val augmented = map + binding
-        augmented.mapValues(v => rewriter(v))
-        new Substitution(augmented)
-    }
-  }
-
-  def + (other: Substitution): Substitution = map.foldLeft(other) { (subst, binding) => subst + binding }
-
-  def unify(t: Type, s: Type): Substitution = t match {
-    case tv: TVar => this + (tv -> s)
-    case _ => this
-  }
 
   def apply(t: Type): Type = t match {
     case v: TVar => map.getOrElse(v, t)
     case TFun(from, to) => TFun(this(from), this(to))
     case _ => t
   }
+
+  def rangeContains(variable: TVar): Boolean = map contains variable
+
+  def composedWith(another: Substitution): Substitution = {
+    val applied = another.map map { case (v, t) => (v, this(t)) }
+    val merged = applied ++ (map filter { case (v, _) => !another.rangeContains(v) })
+    new Substitution(merged)
+  }
+
+  override def toString: String = "{" + map.map { case (v, t) => v + " = " + t }.mkString(", ") + "}"
 }
 
 object Substitution {
   val empty = new Substitution(Map.empty)
 
+  /**
+    * Create a new instance mapping the specified variable to the specified type.
+    */
   def apply(v: TVar, t: Type) = new Substitution(Map(v -> t))
 }
